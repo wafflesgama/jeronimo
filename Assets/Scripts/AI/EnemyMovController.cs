29839ceaa43cf183490617ef381e42de35327e75
Pedro Azevedo <pedrag1234@gmail.com>
2022-04-27 19:34:08 +0100

    refactor: AI movement state machine. feat: pathfinding through maze

--- Assets/Scripts/AI/EnemyMovController.cs
+++ Assets/Scripts/AI/EnemyMovController.cs
@@ -3,56 +3,126 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+
+enum EnemyState
+{
+    IDLE,
+    PATROLLING,
+    FOLLOWING,
+    SEARCHING
+}
+
 public class EnemyMovController : MonoBehaviour
 {
-    
+
+    public float defaultIdleTimer = 3f;
+
     public Transform[] patrolPoints;
+
+    private bool playerDetected = false;
+    private int currentDestination = 0;
+    private float idleTimer;
+    private Vector3 selectedPosition;
+    private NavMeshAgent agent;
+    private EnemyState currentState = EnemyState.PATROLLING;
+    private bool hasSelectedNextPos = false;
     
 
-    private NavMeshAgent agent;
-    private Transform lastKnownPlayerLocation = null;
-    private Vector3 selectedPos;
-    private int currentPoint = 0;
+    
+
 
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(patrolPoints[currentDestination].position);
+        resetIdleTimer();
     }
 
     // Update is called once per frame
     void Update()
     {
-        selectNextPosition();
 
-        Debug.Log(selectedPos);
+        switch (currentState)
+        {
+            case EnemyState.IDLE:
+                if (playerDetected)
+                {
+                    //add Detection Logic Here
+                }
+                else
+                {
 
-        agent.destination = selectedPos;
+
+                    if (!hasSelectedNextPos)
+                    {
+                        selectedPosition = selectNextPatrolPoint();
+                        hasSelectedNextPos = true;
+                    }
+
+                    if (idleTimer > 0)
+                    {
+                        idleTimer -= Time.deltaTime;
+                    }
+                    else
+                    {
+                        agent.SetDestination(selectedPosition);
+                        hasSelectedNextPos=false;
+                        currentState = EnemyState.PATROLLING;
+                    }
+                }
+                break;
+            case EnemyState.PATROLLING:
+                if (playerDetected)
+                {
+                    //add Detection Logic Here
+                }
+                else
+                {
+                    if(!agent.pathPending && agent.remainingDistance < 0.5f)
+                    {
+                        currentState = EnemyState.IDLE;
+                        resetIdleTimer();
+                    }
+                        
+                }
+                break;
+            case EnemyState.FOLLOWING:
+                //add Detection Logic Here
+                break;
+            case EnemyState.SEARCHING:
+                //add Detection Logic Here
+                break;
+        }
+            
     }
 
-    private void selectNextPosition()
+    private Vector3 selectNextPatrolPoint()
     {
-        if(lastKnownPlayerLocation == null)
-        {
-            if(!agent.pathPending && agent.remainingDistance < 0.5f)
-            {
-                if(currentPoint == patrolPoints.Length - 1)
-                    currentPoint = 0;
-                else
-                    currentPoint += 1;
-            }
-            Debug.Log(currentPoint);
+        Vector3 nextPos;
 
-            selectedPos = patrolPoints[currentPoint].position; 
-            
+        if(currentDestination == patrolPoints.Length - 1)
+        {
+           currentDestination = 0;
         }
         else
         {
-            selectedPos = lastKnownPlayerLocation.position;
+          currentDestination++;
         }
+
+        Debug.Log(currentDestination);
+
+        nextPos = patrolPoints[currentDestination].position;
+
+        return nextPos;
     }
 
-    private void setLastKnownPlayerLocation(Transform pos)
+    public void setEnemyDetected(bool detection)
     {
-        lastKnownPlayerLocation = pos;
+        playerDetected = detection;
+    }
+
+    private void resetIdleTimer()
+    {
+        idleTimer = defaultIdleTimer;
     }
 }
