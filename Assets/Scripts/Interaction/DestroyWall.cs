e4bf0fc398dfed00e15cc9d54cd86504e9ce4aa7
nunosanto <up201405774@up.pt>
2022-06-27 22:30:43 +0100

    destroy wall script

---
+++ Assets/Scripts/Interaction/DestroyWall.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWall : MonoBehaviour
+{
+    public GameObject Destroyed;
+    public GameObject Normal;
+    private int destroyVelocity = 1;
+
+    private void OnCollisionEnter(Collision collider)
+    {
+        if (collider.gameObject.tag == "Player" && collider.relativeVelocity.magnitude>destroyVelocity)
+        {
+
+            Destroyed.SetActive(true);
+            GameObject tmp=Destroyed.transform.GetChild(1).gameObject;
+            GameObject BrokenWall = tmp.transform.GetChild(0).gameObject;
+
+            foreach (Transform child in BrokenWall.transform)
+            {
+                child.gameObject.GetComponent<Rigidbody>().AddForce(50f, 50f,0,ForceMode.Impulse);
+            }
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
